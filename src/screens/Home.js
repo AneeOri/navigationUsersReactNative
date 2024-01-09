55035335409e616eea1637d7c3effc2d2fffe0a2
LUZ <balon.basket.90@gmail.com>
2024-01-09 15:49:40 -0600

    [.] Update Home Component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,14 +2,14 @@ import { View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 import { useNavigation } from "@react-navigation/native";
 
-export default function Home({navigation}){
+export default function Home(){
    const navigation = useNavigation();
     return(
      <View style={globalStyles.screenContainer}>
       <Text style={globalStyles.title}>Home</Text>
       <Button
-        onPress={() => navigation.navigate("Settings")}
-        title="Settings"
+        onPress={() => navigation.navigate('Settings')}
+        title="S"
       />
      </View>
     );

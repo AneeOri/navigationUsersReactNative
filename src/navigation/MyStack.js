5b04f6b496b3401b812561841ce1b8abb6e0bcc6
LUZ <balon.basket.90@gmail.com>
2024-01-09 17:31:18 -0600

    [.] Update My Stack Navigation Component
    Adding My Bottom Tab to My stack in order to continue with the flow

--- src/navigation/MyStack.js
+++ src/navigation/MyStack.js
@@ -1,29 +1,32 @@
 import { createStackNavigator } from "@react-navigation/stack";
 import CustomHeader from "../components/CustomHeader";
-import Home from "../screens/Home";
 import Settings from "../screens/Settings";
 import { Text, View } from "react-native";
 import { Colors } from "../constants/colors";
+import MyButtonTab from "./MyButtonTab";
 
 const HomeStack = createStackNavigator();
+
+const myConfig = {
+    headerShown:false,
+    headerTitleAlign:'center',
+    presentation:'card',
+    header : ({navigation, route, options, back}) => (
+        <CustomHeader title={route.name}/>
+    ),
+};
 
 export default function MyStack(){
     /** the name atribute must be unique */
     return(
        <HomeStack.Navigator
         initialRouteName="Home"
-        screenOptions={{
-            headerShown:true,
-            //title:'something',
-            headerTitleAlign:'center',
-            presentation: 'card',
-            header : ({navigation, route, options, back}) => (
-                <CustomHeader title={route.name}/>
-            ),
-        }}
+        screenOptions={myConfig}
        >
-        <HomeStack.Screen name='Home' component={Home}/> 
-        <HomeStack.Screen name='Settings' component={Settings}/>
+        <HomeStack.Screen name='Root' component={MyButtonTab}/> 
+        <HomeStack.Group screenOptions={{headerShown:true}}>
+          <HomeStack.Screen name='Settings' component={Settings}/>
+        </HomeStack.Group>
        </HomeStack.Navigator>
     );
 }

476b5580422acc26b0a8ad9bba76cbe7feef6f1d
LUZ <balon.basket.90@gmail.com>
2024-01-09 14:46:50 -0600

    [.] Update Home Screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,10 +1,16 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
+import { useNavigation } from "@react-navigation/native";
 
-export default function Home(){
+export default function Home({navigation}){
+   const navigation = useNavigation();
     return(
      <View style={globalStyles.screenContainer}>
       <Text style={globalStyles.title}>Home</Text>
+      <Button
+        onPress={() => navigation.navigate("Settings")}
+        title="Settings"
+      />
      </View>
     );
 }

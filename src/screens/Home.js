5ce8d8070c764f83d32e89fa2f208db6c7157adc
LUZ <balon.basket.90@gmail.com>
2024-01-08 20:02:48 -0600

    [.] Update Home Screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,7 +4,7 @@ import { globalStyles } from "../styles/global";
 export default function Home(){
     return(
      <View style={globalStyles.screenContainer}>
-      <Text>Home</Text>
+      <Text style={globalStyles.title}>Home</Text>
      </View>
     );
 }

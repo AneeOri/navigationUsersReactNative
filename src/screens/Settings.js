ba67f53c29b8d65754f0e4429ce00c214f984ed4
LUZ <balon.basket.90@gmail.com>
2024-01-08 20:03:06 -0600

    [.] Update Settings Screen

--- src/screens/Settings.js
+++ src/screens/Settings.js
@@ -1,10 +1,11 @@
 import { Text, View } from "react-native";
+import { globalStyles } from "../styles/global";
 
 
 export default function Settings(){
     return(
-        <View>
-            <Text>Settings</Text>
+        <View style={globalStyles.screenContainer}>
+            <Text style={{backgroundColor:'orange'}}>Settings</Text>
         </View>
     )
 }

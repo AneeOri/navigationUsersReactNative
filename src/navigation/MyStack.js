614fa4c19d7a734a3c3fa6c40fb3911b412928ae
LUZ <balon.basket.90@gmail.com>
2024-01-09 15:49:25 -0600

    [.] Update MyStack component
    Adding Screen Options and Screens

--- src/navigation/MyStack.js
+++ src/navigation/MyStack.js
@@ -1,13 +1,27 @@
 import { createStackNavigator } from "@react-navigation/stack";
+import CustomHeader from "../components/CustomHeader";
 import Home from "../screens/Home";
 import Settings from "../screens/Settings";
+import { Text, View } from "react-native";
+import { Colors } from "../constants/colors";
 
 const HomeStack = createStackNavigator();
 
 export default function MyStack(){
     /** the name atribute must be unique */
     return(
-       <HomeStack.Navigator>
+       <HomeStack.Navigator
+        initialRouteName="Home"
+        screenOptions={{
+            headerShown:true,
+            //title:'something',
+            headerTitleAlign:'center',
+            presentation: 'card',
+            header : ({navigation, route, options, back}) => (
+                <CustomHeader title={route.name}/>
+            ),
+        }}
+       >
         <HomeStack.Screen name='Home' component={Home}/> 
         <HomeStack.Screen name='Settings' component={Settings}/>
        </HomeStack.Navigator>

384c0d8653dfcccce0cf680f9416e3d6f3e06043
LUZ <balon.basket.90@gmail.com>
2024-01-09 17:29:48 -0600

    [.] Update Custom Header Component

--- src/components/CustomHeader.js
+++ src/components/CustomHeader.js
@@ -1,18 +1,27 @@
-import { View, Text } from "react-native";
+import { View, Text, Button } from "react-native";
 import { Colors } from "../constants/colors";
+import { useNavigation } from "@react-navigation/native";
 
 export default function CustomHeader({title}){
+
+  const navigation = useNavigation();
     return(
         <View
           style={{
             height:80,
             width:'100%',
             backgroundColor:Colors.secondary,
-            padding:10,
-            justifyContent:'flex-end',
-            alignItems:'center',
+            flexDirection:'row',
+            justifyContent:'space-between',
+            alignItems:'flex-start',
+            paddingTop:30,
+            paddingRight:150,
           }}
         >
+          <Button
+           title="Home"
+           onPress={()=> navigation.navigate('Home')}
+           />
            <Text
              style={{
                 fontSize:20,

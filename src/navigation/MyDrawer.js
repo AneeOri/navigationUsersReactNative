46351ca5c01a4b6b0000e09693951ce151dada1c
LUZ <balon.basket.90@gmail.com>
2024-01-11 20:22:48 -0600

    [+] Create MyDrawer Component

---
+++ src/navigation/MyDrawer.js
@@ -0,0 +1,19 @@
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import Notifications from "../screens/Notifications";
+import MyStack from "../navigation/MyStack";
+const Drawer = createDrawerNavigator();
+
+export default function MyDrawer(){
+    return(
+        <Drawer.Navigator>
+          <Drawer.Screen
+            name="Stack"
+            component={MyStack}
+            options={{
+            headerShown: false,
+            }}
+          />
+          <Drawer.Screen name="notifications" component={Notifications} />
+        </Drawer.Navigator>
+    );
+}

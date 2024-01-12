e4e02d9eedbdc2db24e1d511e58b0201c56deed1
LUZ <balon.basket.90@gmail.com>
2024-01-11 19:54:34 -0600

    [.] Update MyButtonTab Component

--- src/navigation/MyButtonTab.js
+++ src/navigation/MyButtonTab.js
@@ -1,18 +1,34 @@
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import Home from "../screens/Home";
 import Profile from "../screens/Profile";
+import { AntDesign } from '@expo/vector-icons';
+import { Colors } from "../constants/colors";
 
 const MyTab = createBottomTabNavigator();
 
 export default function MyButtonTab() {
     return(
         <MyTab.Navigator
+        initialRouteName="Home"
          screenOptions={{
-            headerTitleAlign:'center'
+            headerTitleAlign:'center',
+            tabBarInactiveTintColor: Colors.secondary,
          }}
         >
-            <MyTab.Screen name="Home" component={Home}/>
-            <MyTab.Screen name="Profile" component={Profile}/>
+            <MyTab.Screen 
+              name="Home" 
+              component={Home} 
+              options={{
+                tabBarIcon: ({color}) => <AntDesign name="home" size={31} color={color} />
+              }}
+            />
+            <MyTab.Screen 
+              name="Profile" 
+              component={Profile}
+              options={{
+                tabBarIcon: ({color}) => <AntDesign name="user" size={27} color={color} />
+              }}
+            />
         </MyTab.Navigator>
     );
 }

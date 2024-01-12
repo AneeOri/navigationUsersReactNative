9aa97c1841c763311034d8cf748853abc5f61f80
LUZ <balon.basket.90@gmail.com>
2024-01-11 20:23:49 -0600

    [+] Create Screen Notifications

---
+++ src/screens/Notifications.js
@@ -0,0 +1,10 @@
+import { View, Text, Button } from "react-native";
+import { globalStyles } from "../styles/global";
+
+export default function Notifications(){
+    return(
+     <View style={globalStyles.screenContainer}>
+      <Text style={globalStyles.title}>Notifications</Text>
+     </View>
+    );
+}

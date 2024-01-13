8ad2face8a8e120b313c07d36296929000a2356e
LUZ <balon.basket.90@gmail.com>
2024-01-13 09:23:27 -0600

    [+] Create My Top Tab Navigator

---
+++ src/navigation/MyTopTab.js
@@ -0,0 +1,20 @@
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+import Home from "../screens/Home";
+import Contacts from "../screens/Contacts";
+import { Colors } from "../constants/colors";
+
+const Tab = createMaterialTopTabNavigator();
+
+export default function MyTopTab(){
+    return(
+        <Tab.Navigator
+          screenOptions={{
+            tabBarLabelStyle:{color: Colors.secondary},
+            tabBarIndicatorStyle:{backgroundColor: Colors.secondary},
+          }}
+        >
+            <Tab.Screen name='Home' component={Home}/>
+            <Tab.Screen name='Contacts' component={Contacts}/>
+        </Tab.Navigator>
+    );
+}

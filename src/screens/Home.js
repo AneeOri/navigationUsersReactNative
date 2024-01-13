ce2d5ed089073c62fe705dc6b9e67d038867c195
LUZ <balon.basket.90@gmail.com>
2024-01-12 21:07:21 -0600

    [.] Update Home Screen
    using useDrawerStatus as practice

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,16 +1,32 @@
 import { View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 import { useNavigation } from "@react-navigation/native";
+import { useDrawerStatus } from "@react-navigation/drawer";
 
 export default function Home(){
    const navigation = useNavigation();
+   const isDrawerOpen = useDrawerStatus();
+
+   /** navigation.openDrawer
+    *  navigation.closeDrawer
+    *  navigation.toggleDrawer
+    */
     return(
      <View style={globalStyles.screenContainer}>
       <Text style={globalStyles.title}>Home</Text>
-      <Button
-        onPress={() => navigation.navigate('Settings')}
-        title="S"
-      />
+      {
+        isDrawerOpen === 'open' ? (
+          <Button
+          title="S"
+        />
+        ) : (
+          <Button
+          onPress={() => navigation.navigate('Settings')}
+          title="S"
+        />
+        )
+      }
+     
      </View>
     );
 }

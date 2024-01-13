a63681896f0c4d373454b7fad3086d84b5a1203c
LUZ <balon.basket.90@gmail.com>
2024-01-12 20:55:09 -0600

    [.] Update MyButtonTab Component
    Adding a headerLeft button to open drawer

--- src/navigation/MyButtonTab.js
+++ src/navigation/MyButtonTab.js
@@ -3,10 +3,13 @@ import Home from "../screens/Home";
 import Profile from "../screens/Profile";
 import { AntDesign } from '@expo/vector-icons';
 import { Colors } from "../constants/colors";
+import { Pressable } from "react-native";
+import { useNavigation } from "@react-navigation/native";
 
 const MyTab = createBottomTabNavigator();
 
 export default function MyButtonTab() {
+  const navigation = useNavigation();
     return(
         <MyTab.Navigator
         initialRouteName="Home"
@@ -19,6 +22,10 @@ export default function MyButtonTab() {
               name="Home" 
               component={Home} 
               options={{
+                headerLeft: () => (
+                <Pressable onPress={() => navigation.openDrawer()}>
+                   <AntDesign name="menuunfold" style={{marginLeft:15}} size={31} color={Colors.secondary} />
+                </Pressable>),
                 tabBarIcon: ({color}) => <AntDesign name="home" size={31} color={color} />
               }}
             />

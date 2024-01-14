2e8a76ff5627f7ae979a9126c3d11a69193c8db7
LUZ <balon.basket.90@gmail.com>
2024-01-14 10:48:11 -0600

    [.] Update Sign Up Component

--- src/screens/authScreens/SignUp.js
+++ src/screens/authScreens/SignUp.js
@@ -1,10 +1,12 @@
 import { View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
+import MyInput from "../../components/MyInput";
 
 export default function SignUp(){
     return(
      <View style={globalStyles.screenContainer}>
       <Text style={globalStyles.title}>SignUp</Text>
+      <MyInput label={'Email'}/>
      </View>
     );
 }

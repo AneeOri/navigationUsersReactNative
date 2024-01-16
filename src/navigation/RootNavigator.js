905cdd2dcfafa65867d8829b7cb58b75d23e1942
LUZ <balon.basket.90@gmail.com>
2024-01-15 20:39:24 -0600

    [.]  Update Root Navigator Component
    showing components as the case may be

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -1,10 +1,13 @@
 import { NavigationContainer } from "@react-navigation/native";
 import MyDrawer from "./MyDrawer";
+import SignUp from "../screens/authScreens/SignUp";
+import AuthStack from "./authStack/AuthStack";
 
 export default function RootNavigator() {  /**Render the root navigator in App */
+   const user = true;
     return(
         <NavigationContainer>
-           <MyDrawer/>
+           {user ? <MyDrawer/> : <AuthStack/>}
         </NavigationContainer>
     );
 } 

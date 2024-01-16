8180bb4b2fe7fe8f8757ef8f64c8d26d102fbe6c
LUZ <balon.basket.90@gmail.com>
2024-01-15 20:39:55 -0600

    [.]  Update Login Screen

--- src/screens/authScreens/Login.js
+++ src/screens/authScreens/Login.js
@@ -1,10 +1,18 @@
 import { View, Text, Button } from "react-native";
-import { globalStyles } from "../styles/global";
+import { globalStyles } from "../../styles/global";
+import MyInput from "../../components/MyInput";
+import MyButton from "../../components/MyButton";
+import { useNavigation } from "@react-navigation/native";
 
 export default function Login(){
+    const navigation = useNavigation();
     return(
      <View style={globalStyles.screenContainer}>
       <Text style={globalStyles.title}>Login</Text>
+      <MyInput label={'Email'}/>
+      <MyInput label={'Password'} secureTextEntry/>
+      <MyButton title={'Sign In'}/>
+      <MyButton title={'Sign Up'} onPress={() => navigation.navigate('SignUp')}/>
      </View>
     );
 }

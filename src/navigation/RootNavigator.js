040f8bf09f02349fef2b41674ca37a240a4e5b97
LUZ <balon.basket.90@gmail.com>
2024-01-16 20:44:24 -0600

    [.] Update Root Navigator

--- src/navigation/RootNavigator.js
+++ src/navigation/RootNavigator.js
@@ -2,12 +2,37 @@ import { NavigationContainer } from "@react-navigation/native";
 import MyDrawer from "./MyDrawer";
 import SignUp from "../screens/authScreens/SignUp";
 import AuthStack from "./authStack/AuthStack";
+import { useSelector, useDispatch } from "react-redux";
+import { restoreToken } from "../features/auth/auth";
+import { useEffect } from "react";
+import Splash from "../screens/Splash";
 
 export default function RootNavigator() {  /**Render the root navigator in App */
-   const user = true;
+   const {userToken, isLoading} = useSelector(state => state.auth);
+   const dispatch = useDispatch();
+
+   useEffect(() => {
+    getValue();
+   },[]);
+
+   async function getValue() {
+    try {
+         const value = await AsyncStorage.getItem('@token');
+         if(value!== null){
+            console.log('data restored', value);
+            dispatch(restoreToken(value));
+         }else{
+            console.log('no data');
+            dispatch(restoreToken(null));
+         }
+    } catch (error) {
+        console.log(error);
+    }
+}
+    if(isLoading) return <Splash/>;
     return(
         <NavigationContainer>
-           {user ? <MyDrawer/> : <AuthStack/>}
+           {userToken ? <MyDrawer/> : <AuthStack/>}
         </NavigationContainer>
     );
 } 

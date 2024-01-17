cb3837395ad56811309bdb1ddfc2f2ae7f67cbe4
LUZ <balon.basket.90@gmail.com>
2024-01-16 20:56:46 -0600

    [.] Update My Input Component

--- src/components/MyInput.js
+++ src/components/MyInput.js
@@ -1,5 +1,5 @@
-import { TextInput, View } from "react-native";
-
+import { TextInput, Text, View, StyleSheet } from 'react-native';
+import { Colors } from '../constants/colors';
 
 export default function MyInput({label,value,onChangeText,secureTextEntry,}){
 
@@ -9,7 +9,7 @@ export default function MyInput({label,value,onChangeText,secureTextEntry,}){
      * secureTextEntry, textInput included property, could be active or not*/
 
     return(
-       <View>
+       <View style ={styles.container}>
         <TextInput
           placeholder={label}
           value={value}
@@ -19,3 +19,17 @@ export default function MyInput({label,value,onChangeText,secureTextEntry,}){
        </View>
     );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    width: '90%',
+    height: 45,
+    justifyContent: 'center',
+    margin: 10,
+    padding: 10,
+    backgroundColor: Colors.ligth,
+    borderRadius: 10,
+    borderWidth: 1,
+    borderColor: Colors.primary,
+  },
+});

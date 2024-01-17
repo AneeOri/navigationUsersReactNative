2ffae8b5ab4494c053167937ee71f434f44f75ec
LUZ <balon.basket.90@gmail.com>
2024-01-16 20:42:57 -0600

    [.] Update App
    adding the redux provider

--- App.js
+++ App.js
@@ -1,8 +1,12 @@
 import RootNavigator from './src/navigation/RootNavigator';
+import { Provider } from 'react-redux';
+import { store } from './src/app/store';
 
 export default function App() {
   return (
-    <RootNavigator/>
+    <Provider store={store}>
+      <RootNavigator/>
+    </Provider>
   );
 }
 

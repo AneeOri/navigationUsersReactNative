75ff0fc2cc779924a8e35536b828a5efecaa62b3
LUZ <balon.basket.90@gmail.com>
2024-01-17 20:54:03 -0600

    [.] Update Home Component

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,6 +2,7 @@ import { View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 import { useNavigation } from "@react-navigation/native";
 import { useDrawerStatus } from "@react-navigation/drawer";
+import Card from "../components/Card";
 
 export default function Home(){
    const navigation = useNavigation();
@@ -13,20 +14,7 @@ export default function Home(){
     */
     return(
      <View style={globalStyles.screenContainer}>
-      <Text style={globalStyles.title}>Home</Text>
-      {
-        isDrawerOpen === 'open' ? (
-          <Button
-          title="S"
-        />
-        ) : (
-          <Button
-          onPress={() => navigation.navigate('Settings')}
-          title="S"
-        />
-        )
-      }
-     
+       <Card/>
      </View>
     );
 }

d8e7dc8ea50da8d59d2618458477f84a19b6d312
LUZ <balon.basket.90@gmail.com>
2024-01-18 21:48:10 -0600

    [+] Create Add Contact Component

---
+++ src/components/AddContact.js
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import { View, Button, StyleSheet } from "react-native";
+
+export default function AddContact(){
+    const [name, setName] = useState('');
+    
+    return(
+        <View>
+
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    container:{},
+});

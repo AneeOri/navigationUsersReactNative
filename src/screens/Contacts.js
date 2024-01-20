aed8429861a2f66209ccd38f07acacf86f06ec2d
LUZ <balon.basket.90@gmail.com>
2024-01-19 21:56:26 -0600

    [.] Update Contacts Screen

--- src/screens/Contacts.js
+++ src/screens/Contacts.js
@@ -2,22 +2,14 @@ import { View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 import { useNavigation } from "@react-navigation/native";
 import { useEffect } from "react";
+import AddContact from "../components/AddContact";
 
 export default function Contacts(){
     const navigation = useNavigation();
 
-    useEffect(() => {
-        const unsubscribe = navigation.addListener('tabLongPress' , e => {
-            console.log(e);
-        });
-
-        return unsubscribe;
-    },[navigation]);
-
     return(
-     <View style={globalStyles.screenContainer}>
-      <Text style={globalStyles.title}>Contacts</Text>
-      <Button title='home' onPress={() => navigation.jumpTo('Home', {name: 'Lux'})}/>
+     <View style={globalStyles.simpleContainer}>
+      <AddContact/>
      </View> 
     );
 }

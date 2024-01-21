2edcce843ecf9ac965a785ea7ec107fb6f947bef
LUZ <balon.basket.90@gmail.com>
2024-01-21 13:39:33 -0600

    [+] Create Contact List

---
+++ src/components/ContactList.js
@@ -0,0 +1,22 @@
+import { ScrollView, View } from "react-native";
+
+export default function ContactList({contacts,}){
+    return(
+        <ScrollView>
+            {contacts.map((contact, index) => (
+                <Contact
+                 key={index}
+                 contact={contact.name}
+                />
+            ))}
+        </ScrollView>
+    );
+}
+
+function Contact({contact}){
+    return(
+         <View>
+            <Text>{contact}</Text>
+         </View>
+    );
+}

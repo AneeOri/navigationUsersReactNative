21ba5791c487de4d934f972635bd5ac07c5fe08b
LUZ <balon.basket.90@gmail.com>
2024-01-21 13:40:09 -0600

    [.] Update Contacts Screen

--- src/screens/Contacts.js
+++ src/screens/Contacts.js
@@ -1,19 +1,28 @@
 import { View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 import { useNavigation } from "@react-navigation/native";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import AddContact from "../components/AddContact";
+import ContactList from "../components/ContactList";
 
 export default function Contacts(){
+
     const navigation = useNavigation();
+    const [contacts, setContacts] = useState(initialContacts);
 
     return(
      <View style={globalStyles.simpleContainer}>
       <AddContact/>
+      <ContactList contacts={contacts}/>
      </View> 
     );
 }
 
+const initialContacts = [
+    {id:0, name: 'Tato Lee'},
+    {id:1, name: 'Hisa Hio'},
+    {id:2, name: 'Kanani Lokelani'},
+]
 //Before
 /*export default function Contacts(){
     const navigation = useNavigation();

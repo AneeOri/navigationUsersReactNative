8a41031e3c96109e2d48587dc6d5ee6404e8a252
LUZ <balon.basket.90@gmail.com>
2024-01-22 14:07:22 -0600

    [.] Update Contact Reducer Functions

--- src/Reducers/contactReducer.js
+++ src/Reducers/contactReducer.js
@@ -1,14 +1,15 @@
 
-function contactReducer(contacts, action){
+export function contactReducer(contacts, action){
     switch(action.type){
         case 'ADD':{
              return [...contacts, {id:action.id, name: action.name}];
         }
         case 'DELETE':{
-             return contacts.filter(c => c.id ==! action.id);
+             return contacts.filter(c => c.id !== action.id);
         }
         case 'CHANGE':{
-             return contacts.map(c => (c.id === action.c.id ? action.c : c))
+             return contacts.map(contact =>
+                contact.id === action.contact.id ? action.contact : contact);
         }
         default:{
             throw new Error('Unknown action type: ' + action.type);

1ac5f2e354c4ad2a50bf83e61f92f843d7782b7b
LUZ <balon.basket.90@gmail.com>
2024-01-23 13:15:34 -0600

    [.] Update ContactsCont Screen
    Using context

--- src/screens/ContactsCont.js
+++ src/screens/ContactsCont.js
@@ -1,1 +1,17 @@
 /**Using Context */
+import { View } from 'react-native';
+import {ContactsProvider} from '../context/contactsContext';
+import AddContact from '../components/AddContact';
+import ContactList from '../components/ContactList';
+import { globalStyles } from '../styles/global';
+
+export default function ContactsCont(){
+    return(
+        <ContactsProvider>
+          <View style={globalStyles.simpleContainer}>
+            <AddContact/>
+            <ContactList/>
+          </View>
+        </ContactsProvider>
+    );
+}

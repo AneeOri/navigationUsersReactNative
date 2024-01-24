96baff18d1c04ec29167490a21ee28a5a69ed49c
LUZ <balon.basket.90@gmail.com>
2024-01-24 07:46:11 -0600

    [+] Create Contacts Redux Screen

---
+++ src/screens/ContactsRedux.js
@@ -0,0 +1,10 @@
+import { View, Text } from "react-native";
+import { globalStyles } from "../styles/global";
+
+export default function ContactsRedux(){
+    return(
+        <View>
+           <Text>Contacts Redux</Text>
+        </View>
+    );
+}

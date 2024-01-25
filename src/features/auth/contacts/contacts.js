51524403d22805d30a230643eaa316016843d7f1
LUZ <balon.basket.90@gmail.com>
2024-01-25 07:43:46 -0600

    [.] Update Contacts Slice
    Adding reducers

--- src/features/auth/contacts/contacts.js
+++ src/features/auth/contacts/contacts.js
@@ -8,9 +8,17 @@ const contactsSlice = createSlice({
     name: 'contacts',
     initialState,
     reducers: { /**allow to change state from anywhere reducers are functions*/
-       
+       setContacts : (state, action) => {
+           state = action.payload;
+       },
+       addContacts : (state, action) => {
+           state.push(action.payload);
+       },
+       deleteContacts: (state, action) => {
+           state = state.filter(contact => contact.id !== action.payload.id);
+       },
     },
 });
 
-export const {} = contactsSlice.actions;
+export const {setContacts, addContacts, deleteContacts} = contactsSlice.actions;
 export default contactsSlice.reducer;

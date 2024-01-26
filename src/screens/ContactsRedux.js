fa4fdfb37d74f1f2fbf9baf4194dbb3226e53ef6
LUZ <balon.basket.90@gmail.com>
2024-01-26 07:23:03 -0600

    [.] Update Contacts Redux Screen

--- src/screens/ContactsRedux.js
+++ src/screens/ContactsRedux.js
@@ -1,10 +1,26 @@
-import { View, Text } from "react-native";
+import { View, Text, Pressable } from "react-native";
 import { globalStyles } from "../styles/global";
+import { UseSelector, useDispatch, useSelector } from "react-redux";
+import { deleteContacts } from "../features/auth/contacts/contacts";
+
+/**useSelector para acceder al estado */
+/**useDispatch para cambiar el estado, se inciializa porque es una funcion*/
 
 export default function ContactsRedux(){
+    const contacts = useSelector(state => state.contacts);
+    const dispatch = useDispatch();
+
+    function handleLongPress(id){
+        dispatch(deleteContacts(id));
+    }
     return(
-        <View>
-           <Text>Contacts Redux</Text>
+        <View style={globalStyles.simpleContainer}>
+           <Text style={globalStyles.title}>Contacts Redux</Text>
+           {contacts.map((i, index)=>(
+             <Pressable key={index} onLongPress={() => handleLongPress(i.id)}>
+                <Text>{i.name}</Text>
+             </Pressable>
+           ))}
         </View>
     );
 }

b685d6711ab026da585b81d86933131316488d83
LUZ <balon.basket.90@gmail.com>
2024-01-28 15:09:41 -0600

    [.] Create Onboarding Screen
    For push notifications

---
+++ src/screens/Onboarding.js
@@ -0,0 +1,53 @@
+import { Text, View,Image, StyleSheet, TouchableOpacity, Dimensions } from "react-native";
+import { useNavigation } from "@react-navigation/native";
+import { Colors } from "../constants/colors";
+
+export default function Onboarding(){
+ return(
+    <View>
+
+    </View>
+ );
+}
+
+const androidHeight = Dimensions.get('window').height;
+
+const styles = StyleSheet.create({
+    container:{
+        flex:1,
+        backgroundColor: Colors.light,
+        alignItems:'center',
+        justifyContent:'center',
+    },
+    title:{
+        fontSize:32,
+        fontWeight:'bold',
+        marginBottom: androidHeight > 800 ? 70 : 50,
+        marginTop:100,
+        color: Colors.secondary,
+    },
+    subtitle:{
+         fontSize:15,
+         fontWeight:'bold',
+         lineHeight:22,
+         color: Colors.secondary,
+    },
+    subHeadLine:{
+         fontSize:15,
+         fontWeight:'400',
+         lineHeight:20,
+         color: Colors.dark,
+    },
+    featureContainer:{
+          flexDirection:'row',
+          alignItems:'center',
+          paddingHorizontal:20,
+          marginBottom:30,
+    },
+    icon:{
+        width:42,
+        height:42,
+        marginRight:20,
+        resizeMode:'contain',
+    },
+})

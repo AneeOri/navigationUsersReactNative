11748ac572f46b9166a509480c04ad998827a901
LUZ <balon.basket.90@gmail.com>
2024-01-29 21:36:56 -0600

    [.] Update App

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import RootNavigator from './src/navigation/RootNavigator';
 import { Provider } from 'react-redux';
 import { store } from './src/app/store';
+import * as Notifications from 'expo-notifications';
+
+Notifications.setNotificationHandler({
+  handleNotification: async () => ({
+    // if we are in the app, will show a notification or not
+    shouldPlaySound: true,
+    shouldSetBadge:true,
+    shouldShowAlert: true,
+  }),
+});
 
 export default function App() {
   return (

2c9cc0f0c0e57e5089717d7da1eabb0bf2082206
LUZ <balon.basket.90@gmail.com>
2024-01-29 21:26:22 -0600

    [.] Update Home Screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -2,7 +2,9 @@ import { View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
 import { useNavigation } from "@react-navigation/native";
 import { useDrawerStatus } from "@react-navigation/drawer";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import Card from "../components/Card";
+import { useEffect } from "react";
 
 export default function Home(){
    const navigation = useNavigation();
@@ -12,6 +14,21 @@ export default function Home(){
     *  navigation.closeDrawer
     *  navigation.toggleDrawer
     */
+
+   useEffect(() => {
+    chackFirstLauch();
+   },[])
+
+   async function chackFirstLauch(){
+    const firstLaunch = await AsyncStorage.getItem('@firsiLaunch');
+    if(firstLaunch){
+      return;
+    }else{
+      await AsyncStorage.setItem('@firstLauch', 'true');
+      navigation.navigate('Onboarding');
+    }
+   }
+
     return(
      <View style={globalStyles.screenContainer}>
        <Card/>

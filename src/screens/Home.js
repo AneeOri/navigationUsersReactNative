92a859f6c77c8a5d1a14f8896542f5483af03b64
LUZ <balon.basket.90@gmail.com>
2024-01-30 18:06:59 -0600

    [.] Update Home Screen

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -4,12 +4,16 @@ import { useNavigation } from "@react-navigation/native";
 import { useDrawerStatus } from "@react-navigation/drawer";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import Card from "../components/Card";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
+import * as Notification from 'expo-notifications';
+import MyButton from "../components/MyButton";
+import DateTimePicker from "@react-native-community/datetimepicker";
 
 export default function Home(){
    const navigation = useNavigation();
    const isDrawerOpen = useDrawerStatus();
-
+   const [date, setDate] = useState(new Date());
+ 
    /** navigation.openDrawer
     *  navigation.closeDrawer
     *  navigation.toggleDrawer
@@ -17,7 +21,27 @@ export default function Home(){
 
    useEffect(() => {
     chackFirstLauch();
-   },[])
+   },[]);
+
+   async function handleNotification(){
+    const trigger = new Date(date).getTime() +24 *60 *60 *1000;
+      try {
+        const id = await Notification.scheduleNotificationAsync({
+          content:{
+            title: 'Code with anee',
+            body: 'test',
+          },
+          trigger,
+        //  trigger:{
+        //    seconds: 5,
+        //    repeats: false,
+        //  },
+        });
+        alert(`Notification scheduled! ${id} `);
+      } catch (error) {
+        alert('verify date');
+      }
+   }
 
    async function chackFirstLauch(){
     const firstLaunch = await AsyncStorage.getItem('@firsiLaunch');
@@ -31,6 +55,15 @@ export default function Home(){
 
     return(
      <View style={globalStyles.screenContainer}>
+      <DateTimePicker
+        value={date}
+        style={{width: '25%'}}
+        mode={'time'}
+        onChange={(event, selectedDate) => {
+          setDate(selectedDate);
+        }}
+      />
+      <MyButton title={'Schedule Notifications'} onPress={handleNotification}/>
        <Card/>
      </View>
     );

5c3c8eb8ae9e409843d9a5d56a9078fb72e8e87b
LUZ <balon.basket.90@gmail.com>
2024-01-30 18:23:02 -0600

    [.] Update Notifications Screen

--- src/screens/Notifications.js
+++ src/screens/Notifications.js
@@ -1,10 +1,37 @@
 import { View, Text, Button } from "react-native";
 import { globalStyles } from "../styles/global";
+import MyInput from "../components/MyInput";
+import MyButton from "../components/MyButton";
+import { useReducer } from "react";
+
+//function reducer(state, action) { //action is an object
+function reducer(state, {type, title, body, to}) { 
+    switch(type){
+      case 'SET_TITLE': {
+        return{...state, title};
+      }
+      case 'SET_BODY' : {
+        return {...state, body};
+      }
+      default:
+    }
+}
 
 export default function Notifications(){
+
+   const [state, dispatch] = useReducer(reducer, {
+    to:'',
+    sound:'default',
+    title: '',
+    body: '',
+    data:{}
+   })
+
     return(
      <View style={globalStyles.screenContainer}>
       <Text style={globalStyles.title}>Notifications</Text>
+      <MyInput label={'Push Token'}/>
+      <MyButton title={'Send'}/>
      </View>
     );
 }
